034d1c6efccabe036fb713d802a82cc47eaa4248
cat <kittynekostudio@gmail.com>
2024-10-23 20:13:55 -0700

    Basic arithmetic operators.

--- cute-lib/src/lib.rs
+++ cute-lib/src/lib.rs
@@ -1,5 +1,104 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, PartialEq)]
+struct Number(i32);
+
+#[derive(Debug, PartialEq)]
+enum Operation {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+}
+
+#[derive(Debug, PartialEq)]
+enum Value {
+    Number(Number),
+}
+
+#[derive(Debug, PartialEq)]
+struct Expression {
+    lhs: Number,
+    op: Operation,
+    rhs: Number,
+}
+
+impl Expression {
+    pub fn new(string: &str) -> (&str, Self) {
+        let (string, lhs) = Number::new(string);
+        let (string, op) = Operation::new(string);
+        let (string, rhs) = Number::new(string);
+
+        (
+            string,
+            Self {
+                rhs: rhs,
+                op: op,
+                lhs: lhs,
+            },
+        )
+    }
+}
+
+impl Number {
+    pub fn new(string: &str) -> (&str, Self) {
+        let (string, number) = extract_number(string);
+        (string, Number(number.parse().unwrap()))
+    }
+}
+
+impl Operation {
+    pub fn new(string: &str) -> (&str, Self) {
+        let (string, op) = extract_operation(string);
+        (string, op)
+    }
+}
+
+fn extract_operation(string: &str) -> (&str, Operation) {
+    let (string, _) = extract_whitespace(string);
+    let op = match &string[0..1] {
+        "+" => Operation::Addition,
+        "-" => Operation::Subtraction,
+        "*" => Operation::Multiplication,
+        "/" => Operation::Division,
+        _ => panic!("unreachable"),
+    };
+    let string = &string[1..];
+
+    (string, op)
+}
+
+fn extract_number(string: &str) -> (&str, &str) {
+    let (string, _) = extract_whitespace(string);
+    let number_end = string
+        .char_indices()
+        .find_map(|(index, char)| {
+            if char.is_ascii_digit() {
+                return None;
+            } else {
+                return Some(index);
+            }
+        })
+        .unwrap_or_else(|| string.len());
+
+    let number = &string[..number_end];
+    let remainder = &string[number_end..];
+    (remainder, number)
+}
+
+fn extract_whitespace(string: &str) -> (&str, &str) {
+    let whitespace_end = string
+        .char_indices()
+        .find_map(|(index, char)| {
+            if char.is_whitespace() {
+                return None;
+            } else {
+                return Some(index);
+            }
+        })
+        .unwrap_or_else(|| string.len());
+
+    let remainder = &string[whitespace_end..];
+    let whitespace = &string[..whitespace_end];
+    (remainder, whitespace)
 }
 
 #[cfg(test)]
@@ -7,8 +106,41 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn parse_number() {
+        assert_eq!(Number::new("5"), ("", Number(5)));
+    }
+    #[test]
+    fn parse_add() {
+        assert_eq!(Operation::new("+"), ("", Operation::Addition));
+    }
+    #[test]
+    fn parse_sub() {
+        assert_eq!(Operation::new("-"), ("", Operation::Subtraction));
+    }
+    #[test]
+    fn parse_mul() {
+        assert_eq!(Operation::new("*"), ("", Operation::Multiplication));
+    }
+    #[test]
+    fn parse_div() {
+        assert_eq!(Operation::new("/"), ("", Operation::Division));
+    }
+    #[test]
+    fn parse_expression() {
+        assert_eq!(
+            Expression::new("5 + 3"),
+            (
+                "",
+                Expression {
+                    lhs: Number(5),
+                    op: Operation::Addition,
+                    rhs: Number(3)
+                }
+            )
+        );
+    }
+    #[test]
+    fn parse_whitespace() {
+        assert_eq!(Number::new("   5"), ("", Number(5)));
     }
 }
---
+++ docs/examples/grammer.md
@@ -0,0 +1,37 @@
+immutable variables with
+`let =`
+
+mutable variables with
+`mut let = `
+
+### Arithmatic Operations
+
+#### Addition `+`
+Example
+```rust
+1 + 2 // 3
+```
+
+#### Subtraction `-`
+Example
+```rust
+9 - 4 // 5
+```
+
+#### Multiplication `*`
+Example
+```rust
+7 * 2 // 14
+```
+
+#### Division `/`
+Example
+```rust
+4 / 4 // 1
+```
+
+#### Modular `%`
+Example
+```rust
+6 % 3 // 1
+```
